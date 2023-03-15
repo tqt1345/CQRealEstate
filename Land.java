98512df91d76bf7d40d8b971797df103d443472a
tqt1345 <tqt1345@gmail.com>
2023-03-16 00:44:08 +1100

    populate Land class

--- Land.java
+++ Land.java
@@ -1,2 +1,14 @@
 public class Land {
+    private static int lotNumber;
+    private static String address;
+    private static float landArea;
+
+    public Land(int lotNumber, String address, float landArea) {
+        this.lotNumber = lotNumber;
+        this.address = address;
+        this.landArea = landArea;
+    }
+    public setLotNumber(int lotNumber){
+
+    }
 }
